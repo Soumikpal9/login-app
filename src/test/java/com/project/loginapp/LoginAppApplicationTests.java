fcc532db9fbaa7464536b7dc64dba8cc2aab527b
Soumik Pal <soumikpal1997@gmail.com>
2023-11-20 20:46:02 +0530

    added integration tests for register and login

--- src/test/java/com/project/loginapp/LoginAppApplicationTests.java
+++ src/test/java/com/project/loginapp/LoginAppApplicationTests.java
@@ -1,13 +1,59 @@
 package com.project.loginapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.loginapp.dto.LoginDto;
+import com.project.loginapp.dto.UserDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class LoginAppApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void registerUserTest() throws Exception {
+		UserDto userDto = new UserDto("User", "user@test.com", "password");
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/v1/user/register")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(userDto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn();
+
+		String responseBody = mvcResult.getResponse().getContentAsString();
+		Assertions.assertThat(responseBody).isNotEmpty();
+	}
+
+	@Test
+	@Order(2)
+	void loginUserTest() throws Exception {
+		LoginDto loginDto = new LoginDto("user@test.com", "password");
+
+		mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/v1/user/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginDto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Login successful"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(true));
 	}
 
 }
